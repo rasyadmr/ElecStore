3f5797ac10c9092ca5a80ac559e32fbaf64b1b42
rasyadmr <71055821+rasyadmr@users.noreply.github.com>
2024-06-13 16:26:56 +0700

    Add: Migrations & Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function product() {
+        return $this->hasMany(Product::class);
+    }
 }
